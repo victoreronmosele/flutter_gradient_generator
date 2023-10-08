2c5ca7f7d5a1fdbb1d357237422add28c9381d16
Victor Eronmosele <victoreronmosele@gmail.com>
2023-10-08 05:06:12 +0100

    Remove color deletion by pressing backspace/del button
    This was removed since the selected index feature was removed.
    
    The feature and others using keyboard shortcuts will be back.

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_gradient_generator/data/app_dimensions.dart';
-import 'package:flutter_gradient_generator/data/app_typedefs.dart';
 import 'package:flutter_gradient_generator/view_models/gradient_view_model.dart';
 import 'package:flutter_gradient_generator/ui/screens/sections/generator_section.dart';
 import 'package:flutter_gradient_generator/ui/screens/sections/preview_section.dart';
@@ -18,7 +16,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class HomeScreenState extends State<HomeScreen> {
-  final focusNode = FocusNode();
   late GradientViewModel gradientService;
 
   @override
@@ -27,53 +24,7 @@ class HomeScreenState extends State<HomeScreen> {
 
     WidgetsBinding.instance.addPostFrameCallback((_) {
       gradientService = context.read<GradientViewModel>();
-
-      /// This is the event handler that is called when a key is pressed.
-      ///
-      /// The purpose here is to enable the user to delete a [ColorAndStop] by
-      /// pressing the delete or backspace key.
-      HardwareKeyboard.instance
-          .addHandler(_checkKeyEventAndMaybeDeleteColorAndStop);
     });
-  }
-
-  /// Checks if the key event is a delete or backspace key event
-  /// and if so, deletes the currently selected [ColorAndStop].
-  bool _checkKeyEventAndMaybeDeleteColorAndStop(event) {
-    /// If a text field has focus, then the key event is not handled.
-    if (focusNode.hasFocus) return false;
-
-    /// If the event is not a key down event, then the key event is not handled.
-    /// This is so that we don't perform the same deletion multiple times.
-    if (event is! KeyDownEvent) return false;
-
-    final keyPressed = event.logicalKey;
-
-    final deleteOrBackspacePressed = [
-      LogicalKeyboardKey.backspace,
-      LogicalKeyboardKey.delete
-    ].contains(keyPressed);
-
-    if (deleteOrBackspacePressed) {
-      gradientService.deleteSelectedColorAndStopIfMoreThanMinimum(
-          indexToDelete: gradientService.currentSelectedColorIndex);
-    }
-
-    /// If the user presses the delete or backspace key, then `true` is returned
-    /// which prevents the key event from being propagated to the rest of the app.
-    ///
-    /// And if the user presses any other key, then `false` is returned which
-    /// allows the key event to be propagated to the rest of the app.
-    return deleteOrBackspacePressed;
-  }
-
-  @override
-  void dispose() {
-    focusNode.dispose();
-    HardwareKeyboard.instance
-        .removeHandler(_checkKeyEventAndMaybeDeleteColorAndStop);
-
-    super.dispose();
   }
 
   @override
@@ -83,7 +34,6 @@ class HomeScreenState extends State<HomeScreen> {
     final displayPortrait = appDimensions.shouldDisplayPortraitUI;
 
     return Focus(
-      focusNode: focusNode,
       child: Scaffold(
         body: Row(
           crossAxisAlignment: displayPortrait
