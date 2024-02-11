3c79d025a4b48e6108ebd67b08a14f0cfca63786
Victor Eronmosele <victoreronmosele@gmail.com>
2024-02-11 16:05:49 +0100

    Update GradientFactory test

--- test/unit_test/models/gradient_factory_test.dart
+++ test/unit_test/models/gradient_factory_test.dart
@@ -6,39 +6,69 @@ import 'package:flutter_gradient_generator/models/abstract_gradient.dart';
 import 'package:flutter_gradient_generator/models/gradient_factory.dart';
 import 'package:flutter_gradient_generator/models/linear_style_gradient.dart';
 import 'package:flutter_gradient_generator/models/radial_style_gradient.dart';
+import 'package:flutter_gradient_generator/models/sweep_style_gradient.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test(
-      'GradientFactory returns the right AbstractGradient object given a GradientStyle',
-      () {
-    final List<ColorAndStop> colorAndStopList = [
-      (color: const Color(0xFF921E1E), stop: 0),
-      (color: const Color(0xFF0A951F), stop: 100),
-    ];
+  group('GradientFactory ', () {
+    test('returns the right AbstractGradient object given a GradientStyle', () {
+      final List<ColorAndStop> colorAndStopList = [
+        (color: const Color(0xFF921E1E), stop: 0),
+        (color: const Color(0xFF0A951F), stop: 100),
+      ];
 
-    const GradientDirection gradientDirection = GradientDirection.topLeft;
+      const GradientDirection gradientDirection = GradientDirection.topLeft;
 
-    final Map<GradientStyle, AbstractGradient>
-        gradientStyleToAbstractGradientMap = {
-      GradientStyle.linear: LinearStyleGradient(
-          colorAndStopList: colorAndStopList,
-          gradientDirection: gradientDirection),
-      GradientStyle.radial: RadialStyleGradient(
-          colorAndStopList: colorAndStopList,
-          gradientDirection: gradientDirection)
-    };
+      final Map<GradientStyle, AbstractGradient>
+          gradientStyleToAbstractGradientMap = {
+        GradientStyle.linear: LinearStyleGradient(
+            colorAndStopList: colorAndStopList,
+            gradientDirection: gradientDirection),
+        GradientStyle.radial: RadialStyleGradient(
+            colorAndStopList: colorAndStopList,
+            gradientDirection: gradientDirection),
+        GradientStyle.sweep: SweepStyleGradient(
+            colorAndStopList: colorAndStopList,
+            gradientDirection: gradientDirection),
+      };
 
-    gradientStyleToAbstractGradientMap.forEach(
-        (GradientStyle gradientStyle, AbstractGradient abstractGradient) {
+      gradientStyleToAbstractGradientMap.forEach(
+          (GradientStyle gradientStyle, AbstractGradient abstractGradient) {
+        final AbstractGradient actualAbstractGradient = GradientFactory()
+            .getGradient(
+                gradientStyle: gradientStyle,
+                colorAndStopList: colorAndStopList,
+                gradientDirection: gradientDirection);
+
+        final AbstractGradient expectedAbstractGradient = abstractGradient;
+
+        expect(actualAbstractGradient, expectedAbstractGradient);
+      });
+    });
+
+    test('sorts the ColorAndStop list by the stop value', () {
+      final List<ColorAndStop> unsortedColorAndStopList = [
+        (color: const Color(0xFF921E1E), stop: 100),
+        (color: const Color(0xFF0A951F), stop: 0),
+        (color: const Color(0xFF0022FF), stop: 50),
+      ];
+
       final AbstractGradient actualAbstractGradient = GradientFactory()
           .getGradient(
-              gradientStyle: gradientStyle,
-              colorAndStopList: colorAndStopList,
-              gradientDirection: gradientDirection);
-      final AbstractGradient expectedAbstractGradient = abstractGradient;
+              gradientStyle: GradientStyle.linear,
+              colorAndStopList: unsortedColorAndStopList,
+              gradientDirection: GradientDirection.topLeft);
 
-      expect(actualAbstractGradient, expectedAbstractGradient);
+      final actualColorAndStopList =
+          actualAbstractGradient.getColorAndStopList();
+
+      final List<ColorAndStop> expectedColorAndStopList = [
+        (color: const Color(0xFF0A951F), stop: 0),
+        (color: const Color(0xFF0022FF), stop: 50),
+        (color: const Color(0xFF921E1E), stop: 100),
+      ];
+
+      expect(actualColorAndStopList, expectedColorAndStopList);
     });
   });
 }
