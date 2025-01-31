04b7e4898ab25c8d43edc26b2c4db0f7e493627f
Victor Eronmosele <victoreronmosele@gmail.com>
2025-01-31 06:27:19 +0100

    Update service worker to cache requests

---
+++ web/service_worker.js
@@ -0,0 +1,37 @@
+const CACHE_VERSION = "0.0.1";
+const CACHE_NAME = `flutter_gradient_generator_${CACHE_VERSION}`;
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            if (response) {
+                return response;
+            }
+
+            return fetch(event.request).then((response) => {
+                if (!response.ok) {
+                    return response;
+                }
+
+                return caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
